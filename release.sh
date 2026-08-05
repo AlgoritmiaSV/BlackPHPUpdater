8763c108fd4718f01ac452fe637f0f0e180e8100
Edwin Fajardo <contacto@edwinfajardo.com>
2026-08-05 01:25:13 -0600

    Mejoras en blackphp-update, para ejecutar con todos los parámetros existentes

--- release.sh
+++ release.sh
@@ -98,6 +98,7 @@ rsync -cr --delete --chown=fajardo:fajardo --chmod=D755,F644 \
 	--include "public/scripts/maps_antennas.js" \
 	--include "public/scripts/maps_billboards.js" \
 	--include "public/scripts/json_reader.js" \
+	--include "public/scripts/sync_period.js" \
 	--include "public/scripts/*/*.js" \
 	--exclude "public/scripts/*.js" \
 	--exclude "*.html" \
--- update_web.sh
+++ update_web.sh
@@ -2,40 +2,53 @@
 
 # Must be run as root
 if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root"
-	command="$0 $@"
-	su -c "$command"
-	exit 1
+    echo "This script must be run as root"
+    command="$0 $@"
+    su -c "$command"
+    exit 1
 fi
 
-script_path=`realpath "$0"`
-base_path=`dirname "$script_path"`
+script_path=$(realpath "$0")
+base_path=$(dirname "$script_path")
 
 # Task map
 declare -A scripts=(
-	[js]="minify_js.sh"
-	[sass]="sass.sh"
-	[blackphp]="blackphp_sync.sh"
-	[mysqldump]="mysqldump.sh"
-	[language]="language.sh"
-	[images]="images.sh"
+    [js]="minify_js.sh"
+    [sass]="sass.sh"
+    [blackphp]="blackphp_sync.sh"
+    [mysqldump]="mysqldump.sh"
+    [language]="language.sh"
+    [images]="images.sh"
 )
 
-# If no arguments, run all
+# Usage function
+usage() {
+    echo "Usage: blackphp-update [all|${!scripts[@]}]"
+    echo "  all        Run all tasks"
+    echo "  ${!scripts[@]}  Run specific task(s)"
+    exit 1
+}
+
+# If no arguments, show usage
 if [ $# -eq 0 ]; then
-	echo "No arguments provided. Running all tasks..."
-	for key in "${!scripts[@]}"; do
-		echo "Running: ${scripts[$key]}"
-		"$base_path/${scripts[$key]}"
-	done
+    usage
+fi
+
+# If "all" is provided, run all tasks
+if [[ " $* " =~ " all " ]]; then
+    echo "Running all tasks..."
+    for key in "${!scripts[@]}"; do
+        echo "Running: ${scripts[$key]}"
+        "$base_path/${scripts[$key]}"
+    done
 else
-	for task in "$@"; do
-		if [[ ${scripts[$task]+_} ]]; then
-			echo "Running: ${scripts[$task]}"
-			"$base_path/${scripts[$task]}"
-		else
-			echo "Invalid task: $task"
-			echo "Available tasks: ${!scripts[@]}"
-		fi
-	done
+    for task in "$@"; do
+        if [[ ${scripts[$task]+_} ]]; then
+            echo "Running: ${scripts[$task]}"
+            "$base_path/${scripts[$task]}"
+        else
+            echo "Invalid task: $task"
+            usage
+        fi
+    done
 fi
